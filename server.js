e198832681fd9ba2cf4f3237087045cc7ea2650f
prince-chauhan <69704826+prince-chauhan@users.noreply.github.com>
2023-05-12 16:02:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,43 +1,53 @@
 // server.js
-// where your node app starts
 
+// Importing dependencies
 const express = require("express");
-const app = express();
 require('@tensorflow/tfjs');
 require('@tensorflow/tfjs-node');
 const toxicity = require('@tensorflow-models/toxicity');
+
+
+// Initializing Express app
+const app = express();
+
+// Parsing request body as JSON
 app.use(express.json());
 
 
-
+// Serving home.html file to / endpoint
 app.get("/", async function(request, response) {
   // The minimum prediction confidence.
   response.sendFile(__dirname+'/home.html');
 });
 
-
+// Serve main.js file to avoid mimetype issue
 app.get("/main.js", async function(request, response) {
   // The minimum prediction confidence.
   response.header("Content-Type", "application/javascript");
   response.sendFile(__dirname+'/main.js');
 });
+
+// POST route for sentiment analysis
 app.post('/sentiment', async function(req, res){
   try{
-const threshold = 0.9;
-toxicity.load(threshold).then(model => {
-  const sentences = [req.body.message]||['hello'];
-
-  model.classify(sentences).then(predictions => {
-    res.json({sentiments:predictions.filter(obj=>obj.label==='toxicity')[0].results[0]})
-  });
-});
+      const threshold = 0.9;
+      // Load toxicity model
+      toxicity.load(threshold).then(model => {
+        // Classifying input sentences if no message passed then use hello 
+        const sentences = [req.body.message]||['hello'];
+        model.classify(sentences).then(predictions => {
+          
+          // Return resultby filtering out only toxicity from prediction and removing other labels
+          res.json({sentiments:predictions.filter(obj=>obj.label==='toxicity')[0].results[0]})
+      });
+    });
   }
   catch(err){
     res.status(500).json({message:'Internal error occured'})
   }
 });
 
-// listen for requests :)
+// Starting the server
 const listener = app.listen(process.env.PORT, function() {
   console.log("Your app is listening on port " + listener.address().port);
 });
